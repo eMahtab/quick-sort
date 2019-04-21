5125e7c693b6e0a4b1cc0eb8b6dae4290c8756c3
Mahtab Alam <alammahtab08@gmail.com>
2019-04-21 21:56:43 +0530

    Updated README and added the document

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # quick-sort
 Quick Sort Implementation
 
+# Link to Youtube video
+
+[Quick Sort - Explanation and Implementation](https://www.youtube.com/watch?v=4ItzLRAW6CQ "Quick Sort")
+
 ![Quick Sort](quick-sort-example.PNG?raw=true "Quick Sort")
--- src/net/mahtabalam/QuickSort.java
+++ src/net/mahtabalam/QuickSort.java
@@ -7,9 +7,6 @@ class QuickSort
 	public static void main(String args[]) 
 	{ 
 		int arr[] = {10, 80, 30, 90, 40, 50, 70}; 
-//		int arr[] = {10, 7}; 
-//		int arr[] = {7, 10}; 
-//		int arr[] = {10, 11, 77, 45, 56}; 
 		int n = arr.length; 
 		sort(arr, 0, n-1); 
 
